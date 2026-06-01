fbf4235c38bad8489c0f535ae1a66ec8b054d021
tqbinh1268 <tqbinh@gmail.com>
2026-06-01 18:56:49 +0700

    cập nhật lại luồng demo

--- PS12345XTH.BinhTQ/src/main/java/com/fpoly/lab/controller/ProductDbController.java
+++ PS12345XTH.BinhTQ/src/main/java/com/fpoly/lab/controller/ProductDbController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/db/products")
+@CrossOrigin("*") // Mở CORS cho tất cả (hoặc có thể thêm "http://localhost:63342")
 public class ProductDbController {
 
     private final ProductDbService productDbService;
